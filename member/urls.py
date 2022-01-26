623f5e7f30aa1215c06ab782209f77240843f393
foldedSH <luce-in-altis97@naver.com>
2022-01-26 13:52:29 +0900

    2022.01.26-v1

--- member/urls.py
+++ member/urls.py
@@ -6,7 +6,15 @@ urlpatterns = [
     # 직접 구현
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
     path('register/', views.register, name='register'),
+    path("register/check/", views.check_id, name = 'check_id'),
+
     path('login/valid/', views.valid_login, name = 'valid_login'),
+
     path('login/find/', views.find, name = 'find'),
+    path("login/find/userid/", views.find_userid, name = 'find_userid'),
+
+    path('login/reset/', views.reset, name = 'reset'),
+    path("login/reset/userpw/", views.reset_userpw, name = 'reset_userpw'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,5 +1,5 @@
 from django.utils import timezone
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from member.models import UserTable
 from argon2 import PasswordHasher # 패스워드 암호화
@@ -8,30 +8,30 @@ from argon2.exceptions import VerifyMismatchError
 
 # 회원가입 - 직접 구현
 def register(request):
-    if request.method == 'GET':
-        user_id = request.GET.get('user_id')
+    # if request.method == 'GET':
+    #     user_id = request.GET.get('user_id')
 
-        if UserTable.objects.filter(user_id = user_id).exists():
-            return HttpResponse("존재")
+    #     if UserTable.objects.filter(user_id = user_id).exists():
+    #         return HttpResponse("존재")
 
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
         pw = request.POST.get('pw')
         confirm_pw = request.POST.get('double_pw')
         username = request.POST.get('username')
-        email = request.POST.get('email')
+        user_email = request.POST.get('user_email')
 
-        if (user_id=="") or (pw=="") or (confirm_pw=="") or (username=="") or (email==""):
+        if (user_id=="") or (pw=="") or (confirm_pw=="") or (username=="") or (user_email==""):
             return 
 
 
         # 입력 이메일 형식이 잘못되었거나 이미 존재하는 메일인 경우,
-        if UserTable.objects.filter(email = email).exists():
+        if UserTable.objects.filter(user_email = user_email).exists():
             return HttpResponse("이미 가입된 이메일입니다.")
 
         # 1차 패스워드와 2차 패스워드가 동일하면 DB 저장
         if pw == confirm_pw:
-            m = UserTable(user_id=user_id, pw=PasswordHasher().hash(pw), username=username, email = email)
+            m = UserTable(user_id=user_id, pw=PasswordHasher().hash(pw), username=username, user_email = user_email)
             m.register_date = timezone.now()
             m.save()
 
@@ -39,24 +39,18 @@ def register(request):
     
     return render(request, 'member/register.html')
 
+def check_id(request):
+    obj = request.body.decode("utf-8")
+    data = json.loads(obj)
 
-def valid_login(request):
-    user_id = request.GET.get('user_id')
-    pw = request.GET.get('pw')
+    user_id = data['user_id']
+    if UserTable.objects.filter(user_id = user_id).exists():
+        return HttpResponse("존재")
+        
+    return JsonResponse(data)
 
-    # 회원정보 조회 실패 시 예외 발생
-    try:
-        corr_pw = UserTable.objects.get(user_id=user_id).pw # 암호화 된 pw
 
-        try:
-            if PasswordHasher().verify(corr_pw.encode(), pw.encode()): # 패스워드 확인
-                return HttpResponse('Success')
 
-        except VerifyMismatchError as e: # 패스워드 틀린 사용자
-            return HttpResponse('Error1')
-
-    except UserTable.DoesNotExist as e: # 없는 사용자
-        return HttpResponse('Error2')
 
     
 # 로그인 - 직접 구현
@@ -76,6 +70,29 @@ def login(request):
     else:
         return render(request, 'member/login.html')
 
+def valid_login(request):
+    obj = request.body.decode("utf-8")
+    data = json.loads(obj)
+    
+    user_id = data['user_id']
+    pw = data['pw']
+
+    # 회원정보 조회 실패 시 예외 발생
+    try:
+        corr_pw = UserTable.objects.get(user_id=user_id).pw # 암호화 된 pw
+
+        try:
+            if PasswordHasher().verify(corr_pw.encode(), pw.encode()): # 패스워드 확인
+                return HttpResponse('Success')
+
+        except VerifyMismatchError as e: # 패스워드 틀린 사용자
+            return HttpResponse('Error1')
+
+    except UserTable.DoesNotExist as e: # 없는 사용자
+        return HttpResponse('Error2')
+        
+    return JsonResponse(data)
+
 
 def logout(request):
     del request.session['user_id'] # 개별 삭제
@@ -86,16 +103,53 @@ def logout(request):
 
     
 def find(request):
-    if request.method == 'POST':
-        username = request.GET.get('username')
-        user_email = request.GET.get('user_email')
+    return render(request, 'member/finduser.html')
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     user_email = request.POST.get('user_email')
 
-        try:
-            user_id = UserTable.objects.get(username=username, user_email=user_email).user_id
-            find_info = f"{username}님의 ID는 {user_id}입니다."
-            return HttpResponse(find_info)
+    #     try:
+    #         user_id = UserTable.objects.get(username=username, user_email=user_email).user_id
+    #         find_info = f"{username}님의 ID는 {user_id}입니다."
+    #         return HttpResponse(find_info)
 
-        except UserTable.DoesNotExist as e:
-            return HttpResponse('Not Found ID')
+    #     except UserTable.DoesNotExist as e:
+    #         return HttpResponse('Not Found ID')
 
-    return redirect('member:find')
+import json
+def find_userid(request):
+    obj = request.body.decode("utf-8")
+    data = json.loads(obj)
+
+    username = data['username']
+    user_email = data['user_email']
+
+    try:
+        user_id = UserTable.objects.get(username=username, user_email=user_email).user_id
+        find_info = f"{username}님의 ID는 {user_id}입니다."
+        return HttpResponse(find_info)
+
+    except UserTable.DoesNotExist as e:
+        return HttpResponse('입력된 정보를 찾을 수 없습니다.')
+
+
+def reset(request):
+    return render(request, 'member/resetpw.html')
+
+import json
+def reset_userpw(request):
+    obj = request.body.decode("utf-8")
+    data = json.loads(obj)
+
+    user_id = data['user_id']
+    user_email = data['user_email']
+    chg_pw = data['pw']
+
+    try:
+        user = UserTable.objects.get(user_id = user_id, user_email=user_email)
+        user.pw = PasswordHasher().hash(chg_pw)
+        user.save()
+        return HttpResponse("비밀번호가 변경되었습니다.")
+
+    except UserTable.DoesNotExist as e:
+        return HttpResponse('입력된 정보를 찾을 수 없습니다.')
