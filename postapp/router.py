080def0f14aaabc362a159a67ec8bc89cb3fbacc
hyeonsuhan <hyeonsuhan@gmail.com>
2022-01-27 13:18:05 +0900

    fixed

--- postapp/router.py
+++ postapp/router.py
@@ -1,11 +1,11 @@
 class DBRouter:
     def db_for_read(self, model,**hints):
         if model._meta.app_label == 'postapp':
-            return 'posting'
+            return 'default'
         return None
     def db_for_write(self, model,**hints):
         if model._meta.app_label == 'postapp':
-            return 'posting'
+            return 'default'
         return None                         
     def allow_relation(self, obj1, obj2, **hints):
         if obj1._meta.app_label == 'postapp' or \
@@ -14,5 +14,5 @@ class DBRouter:
         return None
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == 'postapp':
-            return db == 'posting'
+            return db == 'default'
         return None
