49a4689b7793881e3b58ab1199e623fe871232c0
joungeun <joungeun@naver.com>
2022-01-27 13:36:05 +0900

    media url

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from app import views
 from main import views
 from postapp import views
 from member import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('member.urls')),
@@ -31,3 +33,4 @@ urlpatterns = [
     path('postapp/', include('postapp.urls')),
     path('member/', include('member.urls'))
 ]
+urlpatterns +=static(settings.MEDIA_URL, document_root  = settings.MEDIA_ROOT)
