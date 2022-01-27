e2f3592cab61e99af42415d0a71b6de8f291b65f
foldedSH <93170319+foldedSH@users.noreply.github.com>
2022-01-27 14:52:08 +0900

    Add files via upload

--- member/views.py
+++ member/views.py
@@ -8,12 +8,6 @@ from argon2.exceptions import VerifyMismatchError
 
 # 회원가입 - 직접 구현
 def register(request):
-    # if request.method == 'GET':
-    #     user_id = request.GET.get('user_id')
-
-    #     if User.objects.filter(user_id = user_id).exists():
-    #         return HttpResponse("존재")
-
     if request.method == 'POST':
         user_id = request.POST.get('user_id')
         pw = request.POST.get('pw')
@@ -24,20 +18,13 @@ def register(request):
         postid = request.POST.get('postid')
         addr = request.POST.get('addr')
 
-        # detail_addr = request.POST.get('detail_addr')
-
-        # if (user_id=="") or (pw=="") or (confirm_pw=="") or (username=="") or (email==""):
-        #     return 
-
-        # 입력 이메일 형식이 잘못되었거나 이미 존재하는 메일인 경우,
-
         if User.objects.filter(email = email).exists():
             return HttpResponse("이미 가입된 이메일입니다.")
 
         # 1차 패스워드와 2차 패스워드가 동일하면 DB 저장
         if pw == confirm_pw:
             m = User(user_id=user_id, pw=PasswordHasher().hash(pw), 
-                            username=username, email = email,  addr = addr)# postid = postid, addr = addr, detail_addr = detail_addr)
+                            username=username, email = email,  addr = addr)
             m.register_date = timezone.now()
             m.save()
             return render(request, 'main/main.html') # 회원가입 성공 후, 메인 페이지로 이동
@@ -72,6 +59,7 @@ def login(request):
         request.session['username'] = m.username
 
         return render(request, 'main/main.html')
+        
     else:
         return render(request, 'member/login.html')
 
@@ -104,22 +92,11 @@ def logout(request):
     del request.session['username'] # 개별 삭제
     request.session.flush() # 전체 삭제
 
-    return redirect('main/main.html') # 메인페이지로 설정
+    return render(request, 'main/main.html')
 
     
 def find(request):
     return render(request, 'member/finduser.html')
-    # if request.method == 'POST':
-    #     username = request.POST.get('username')
-    #     email = request.POST.get('email')
-
-    #     try:
-    #         user_id = User.objects.get(username=username, email=email).user_id
-    #         find_info = f"{username}님의 ID는 {user_id}입니다."
-    #         return HttpResponse(find_info)
-
-    #     except User.DoesNotExist as e:
-    #         return HttpResponse('Not Found ID')
 
 import json
 def find_userid(request):
