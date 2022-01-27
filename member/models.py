0c3c5066eed1a5d35c171c6e05493aff7d76d290
foldedSH <luce-in-altis97@naver.com>
2022-01-27 10:18:41 +0900

    2022.01.27-v1

--- member/models.py
+++ member/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class UserTable(models.Model):
-    user_index = models.AutoField(primary_key=True) # 가입순서
-    username = models.CharField(max_length = 20, null=False) # 사용자 이름
-    user_id = models.CharField(max_length = 20, null=False, unique=True) # 사용자 ID
-    pw = models.CharField(max_length = 20, null=False) # 사용자 PW
-    user_email = models.EmailField(max_length=50, null=False)
-    register_date = models.DateTimeField() # 등록 날짜
+# # Create your models here.
+# class UserTable(models.Model):
+#     user_index = models.AutoField(primary_key=True) # 가입순서
+#     username = models.CharField(max_length = 20, null=False) # 사용자 이름
+#     user_id = models.CharField(max_length = 20, null=False, unique=True) # 사용자 ID
+#     pw = models.CharField(max_length = 20, null=False) # 사용자 PW
+#     user_email = models.EmailField(max_length=50, null=False)
+#     register_date = models.DateTimeField() # 등록 날짜
+
+class User(models.Model):
+   user_index = models.AutoField(primary_key=True) # 가입순서
+   username = models.CharField(max_length = 20, null=False) # 사용자 이름
+   user_id = models.CharField(max_length = 20, null=False, unique=True) # 사용자 ID
+   pw = models.CharField(max_length = 225, null=False) # 사용자 PW
+   addr = models.CharField(max_length=100, null=True)
+   email = models.EmailField(max_length=50, null=False)
+   register_date = models.DateTimeField(auto_now_add=True) # 등록 날짜
+
+   class Meta:
+      db_table = 'User'
+      app_label = 'member'
+      managed = False
